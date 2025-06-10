cb729e524ee96d001eb86b1ebb207460b3c44b56
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 02:05:15 +0500

    Update AC.h

--- AC.h
+++ AC.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef AC_H
+#define AC_H
+#include "Device.h"
+class AC :public Device
+{
+	int temp;
+	char mode;
+public:
+	AC();
+	AC(MyStr _name, bool _isON);
+	AC(const AC& other);
+	AC& operator=(const AC& other);
+	void turnOn() override;
+	void turnOff() override;
+	void SetSetting() override;
+};
+#endif // !AC_H
