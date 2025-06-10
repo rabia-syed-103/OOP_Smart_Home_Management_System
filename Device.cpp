a5818c10fda8eeb957fdde1127255a8ad94ae58f
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 02:08:10 +0500

    Update Device.cpp

--- Device.cpp
+++ Device.cpp
@@ -1,11 +1,43 @@
 #include "Device.h"
 
+Device::Device():isOn(false)
+{
+}
+
+Device::Device(MyStr _name, bool _isOn):name(_name),isOn(_isOn)
+{
+}
+
+Device::Device(const Device& other):name(other.name),isOn(other.isOn)
+{
+}
+
+Device& Device::operator=(const Device& other)
+{
+	if (this != &other)
+	{
+		this->name = other.name;
+		this->isOn = other.isOn;
+	}
+	return *this;
+
+
+}
+
 void Device::turnOn()
 {
+	if (this->isOn == false)
+		this->isOn = true;
+	else
+		cout << "\nAlready On.";
 }
 
 void Device::turnOff()
 {
+	if (this->isOn == true)
+		this->isOn = false;
+	else
+		cout << "\nAlready Off.";
 }
 
 void Device::SetSetting()
