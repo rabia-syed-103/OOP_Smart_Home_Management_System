5dba9958fb36fa348205f04cb95c100aec20c94b
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 02:06:50 +0500

    Update Light.cpp

---
+++ Light.cpp
@@ -0,0 +1,37 @@
+#include "Light.h"
+
+Light::Light():brightness(0)
+{
+}
+Light::Light(MyStr _name, bool _isON) :Device(_name, _isON),brightness(0)
+{
+
+}
+Light::Light(const Light& other) :Device(other)
+{
+	this->brightness = other.brightness;
+}
+Light&Light::operator=(const Light& other)
+{
+	if (this != &other)
+	{
+		Device::operator=(other);
+		this->brightness = other.brightness;
+	}
+	return *this;
+}
+
+void Light::turnOn()
+{
+	this->turnOn();
+}
+void Light::turnOff()
+{
+	this->turnOff();
+
+}
+void Light::SetSetting()
+{
+	cout << "Enter Brightness :: ";
+	cin >> this->brightness;
+}
