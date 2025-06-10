90a4b95ebd80e67273e64454f46e582f0ad4f081
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-10 17:55:04 +0500

    Update Device.h

--- Device.h
+++ Device.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef DEVICE_H
+#define DEVICE_H
+#include "MyStr.h"
+
+class Device
+{
+private:
+	MyStr name;
+	bool isOn;
+public:
+	void turnOn();
+	void turnOff();
+	void SetSetting();
+	MyStr getname();
+
+};
+#endif 
