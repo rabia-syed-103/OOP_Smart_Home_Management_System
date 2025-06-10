2c54f8167f428c0e8f407700576034158434af77
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 02:26:59 +0500

    Update Lock.cpp

---
+++ Lock.cpp
@@ -0,0 +1,49 @@
+#include "Lock.h"
+
+Lock::Lock()
+{
+}
+Lock::Lock(MyStr _name, bool _isON) :Device(_name, _isON)
+{
+
+}
+Lock::Lock(const Lock& other) :Device(other)
+{
+	this->password = other.password;
+}
+Lock& Lock::operator=(const Lock& other)
+{
+	if (this != &other)
+	{
+		Device::operator=(other);
+		this->password = other.password;
+		
+	}
+	return *this;
+}
+
+void Lock::turnOn()
+{
+	cout << "Enter Password to turn on the lock :: ";
+	MyStr pass;
+	cin >> pass;
+	if (pass.is_equal(this->password))
+		this->turnOn();
+	else
+		cout << "\n~UnAuthorized!";
+}
+
+void Lock::turnOff()
+{
+	cout << "Enter Password to turn Off the lock :: ";
+	MyStr pass;
+	cin >> pass;
+	if (pass.is_equal(this->password))
+		this->turnOff();
+	else
+		cout << "\n~UnAuthorized!";
+
+}
+void Lock::SetSetting()
+{
+}
