5a77b6c83725ad3d529c9f3183bfa149fd5e20f6
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 02:47:00 +0500

    Update Sensor.cpp

---
+++ Sensor.cpp
@@ -0,0 +1,37 @@
+#include "Sensor.h"
+
+Sensor::Sensor()
+{
+}
+Sensor::Sensor(MyStr _name, bool _isON) :Device(_name, _isON)
+{
+
+}
+Sensor::Sensor(const Sensor& other) :Device(other)
+{
+}
+Sensor& Sensor::operator=(const Sensor& other)
+{
+	if (this != &other)
+	{
+		Device::operator=(other);
+
+	}
+	return *this;
+}
+
+void Sensor::turnOn()
+{
+	throw MyStr("Sensor cannot be turned on manually.");
+}
+
+void Sensor::turnOff()
+{
+	throw MyStr("Sensor cannot be turned off manually.");
+
+}
+void Sensor::SetSetting()
+{
+	throw MyStr("Settings of Sensor cannot be changed.");
+
+}
