acd54a761895c19bf4099714763822b6380d0070
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 02:17:37 +0500

    Update Fan.h

--- Fan.h
+++ Fan.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef FAN_H
+#define FAN_H
+#include "Device.h"
+class Fan :public Device
+{
+	int speed;
+
+public:
+	Fan();
+	Fan(MyStr _name, bool _isON);
+	Fan(const Fan& other);
+	Fan& operator=(const Fan& other);
+	void turnOn() override;
+	void turnOff() override;
+	void SetSetting() override;
+};
+#endif // !FAN_H
