781db0354bda15492262cb13d4ee442451f84fcf
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 02:07:26 +0500

    Update Light.h

--- Light.h
+++ Light.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef LIGHT_H
+#define LIGHT_H
+#include "Device.h"
+class Light :public Device
+{
+	int brightness;
+	
+public:
+	Light();
+	Light(MyStr _name, bool _isON);
+	Light(const Light& other);
+	Light& operator=(const Light& other);
+	void turnOn() override;
+	void turnOff() override;
+	void SetSetting() override;
+};
+#endif // !LIGHT_H
