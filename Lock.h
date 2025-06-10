e022f45fe7e30c03d57405ccb856084c80e9bb31
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 02:20:48 +0500

    Update Lock.h

--- Lock.h
+++ Lock.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef LOCK_H
+#define LOCK_H
+#include "Device.h"
+class Lock :public Device
+{
+public:
+	Lock();
+	Lock(MyStr _name, bool _isON);
+	Lock(const Lock& other);
+	Lock& operator=(const Lock& other);
+	void turnOn() override;
+	void turnOff() override;
+	void SetSetting() override;
+};
+#endif // !LOCK_H
