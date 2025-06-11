fcf54e7d8b1a03f4610a87254d8fdb1153e82efe
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 15:47:01 +0500

    Update ChildUser.h

--- ChildUser.h
+++ ChildUser.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef CHILDUSER_H
+#define CHILDUSER_H
+#include <iostream>
+#include "MyStr.h"
+#include "User.h"
+class ChildUser :public User
+{
+
+public:
+	ChildUser();
+	ChildUser(MyStr _name);
+	ChildUser(const User& other);
+	ChildUser& operator=(const User& other);
+	bool CanChangeSetting()override;
+	bool CanToggleDevice()override;
+};
+#endif // !CHILDUSER_H
