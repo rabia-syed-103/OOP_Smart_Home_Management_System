2a7197aeddcbffd6c40d32650016779be22f9c42
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 15:57:17 +0500

    Update GuestUser.h

--- GuestUser.h
+++ GuestUser.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef GUESTUSER_H
+#define GUESTUSER_H
+#include <iostream>
+#include "MyStr.h"
+#include "User.h"
+class GuestUser :public User
+{
+
+public:
+	GuestUser();
+	GuestUser(MyStr _name);
+	GuestUser(const User& other);
+	GuestUser& operator=(const User& other);
+	bool CanChangeSetting()override;
+	bool CanToggleDevice()override;
+};
+#endif // !GUESTUSER_H
