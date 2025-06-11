d2c9112eb5715c9e0b33499c1f61c93bb7bbfd36
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-11 19:32:17 +0500

    Update Room.h

--- Room.h
+++ Room.h
@@ -1,1 +1,23 @@
+#ifndef ROOM_H
+#define ROOM_H
+#include "MyStr.h"
+#include "MyVector.h"
+#include "Device.h"
+#include "TemplateFind.h"
+class Room
+{
+	MyStr name;
+	MyVector <Device*> devices;
+public:
+	Room();
+	Room(MyStr _name, MyVector <Device*> _device);
+	Room(MyStr _name);
+	Room(const Room& other);
+	void AddDevice(Device* dev);
+	void RemoveDevice(Device* dev);
+	bool ToggleDevice(MyStr devicename,bool toggle);
+	bool UpdateDeviceSetting(MyStr devicename);
+	MyStr getname();
 
+};
+#endif // !ROOM_H
