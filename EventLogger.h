d263137a4afbd5315ca137350b337789d88134b8
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-13 02:48:02 +0500

    Update EventLogger.h

--- EventLogger.h
+++ EventLogger.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef EVENTLOGGER_H
+#define EVENTLOGGER_H
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "MyStr.h"
+#include <ctime>
+using namespace std;
+class EventLogger {
+private:
+    
+    ofstream logfile;
+  
+public:
+
+    EventLogger();
+    void initialize(const MyStr& filename);
+    void cleanup();
+    void writeError(const MyStr& error_message); 
+    void writeMessage(const MyStr& message);
+
+};
+#endif // !EVENTLOGGER_H
