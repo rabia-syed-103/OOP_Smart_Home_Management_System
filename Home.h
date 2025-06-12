ccc582cac6d159d9f7d913ed79ec09491521d1e2
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-13 00:29:57 +0500

    Update Home.h

--- Home.h
+++ Home.h
@@ -1,1 +1,30 @@
-#pragma once
+#ifndef HOME_H
+#define HOME_H
+#include "User.h"
+#include "Device.h"
+#include "EventLogger.h"
+#include "EnergyMonitor.h"
+#include "MyStr.h"
+#include "MyVector.h"
+#include "Room.h"
+class Home
+{
+	static Home* Main;
+	MyStr name;
+	MyVector <Room*> rooms;
+	MyVector <User*> users;
+	EventLogger eventlog;
+	Home(const MyStr& _name, const string& filename);
+	Home(const Home& other) = delete;
+
+public:
+	static Home* getinstance(const MyStr& _name, const string& filename);
+	void AddUser(User* _user);
+	void AddRoom(Room* _room);
+	void AddDeviceToRoom(int _roomid, Device* _device);
+	bool ToggleDevicePower(int _roomid, int deviceid, MyStr username,bool toggle);
+	bool ChangeSetting(int _roomid, int deviceid, MyStr username);
+
+
+};
+#endif // !HOME_H
