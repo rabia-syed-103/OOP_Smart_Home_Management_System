d78b47eedff71b13424fad0fb8f321c2ad69e3db
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-13 02:48:24 +0500

    Update EnergyMonitor.h

--- EnergyMonitor.h
+++ EnergyMonitor.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef ENERGYMONITOR_H
+#define ENERGYMONITOR_H
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "MyStr.h"
+#include <ctime>
+using namespace std;
+class EnergyMonitor {
+private:
+
+    ofstream logfile;
+
+public:
+
+    EnergyMonitor();
+    void initialize(const MyStr& filename);
+    void cleanup();
+    void writeUpdate(const MyStr& error_message);
+
+};
+#endif // !EVENTLOGGER_H
