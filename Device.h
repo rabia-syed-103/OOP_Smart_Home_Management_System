b6e23d6bef064c91882dcc3e7b64a7c459c1cc38
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-12 17:58:38 +0500

    Update Device.h

--- Device.h
+++ Device.h
@@ -5,13 +5,19 @@
 class Device
 {
 private:
+	int id;
 	MyStr name;
 	bool isOn;
 public:
-	void turnOn();
-	void turnOff();
-	void SetSetting();
+	Device();
+	Device(int _id,MyStr _name, bool _isOn);
+	Device(const Device& other);
+	Device& operator=(const Device& other);
+	virtual void turnOn();
+	virtual void turnOff();
+	virtual void SetSetting();
 	MyStr getname();
 
 };
 #endif 
+
