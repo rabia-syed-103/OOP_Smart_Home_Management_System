a58ee3df994c9a31dcb7ac18ba04b56f5c22c0b3
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-13 02:48:44 +0500

    Update EnergyMonitor.cpp

---
+++ EnergyMonitor.cpp
@@ -0,0 +1,29 @@
+#include "EnergyMonitor.h"
+
+EnergyMonitor::EnergyMonitor()
+{
+}
+
+void EnergyMonitor::initialize(const MyStr& filename) {
+    string _filename = std::string(filename.c_str());
+    logfile.open(_filename);
+    if (!logfile)
+    {
+        cout << "This file can't be opened!";
+    }
+    logfile << "All Energy Updates \n";
+}
+void EnergyMonitor::cleanup() {
+
+    logfile.close();
+
+}
+void EnergyMonitor::writeUpdate(const MyStr& error_message) {
+    time_t timestamp;
+    time(&timestamp);
+
+    char timeStr[26];
+    ctime_s(timeStr, sizeof(timeStr), &timestamp);
+    logfile << "Update Time :: " << timeStr;
+    logfile << "Update :: " << error_message << endl;
+}
