25ea0c47a13dc09b2c5353eeb4ff44fcd0426d28
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-12 18:01:13 +0500

    Update AC.h

--- AC.h
+++ AC.h
@@ -7,11 +7,14 @@ class AC :public Device
 	char mode;
 public:
 	AC();
-	AC(MyStr _name, bool _isON);
+	AC(int _id,MyStr _name, bool _isON);
 	AC(const AC& other);
 	AC& operator=(const AC& other);
 	void turnOn() override;
 	void turnOff() override;
 	void SetSetting() override;
+	~AC();
+
 };
 #endif // !AC_H
+
