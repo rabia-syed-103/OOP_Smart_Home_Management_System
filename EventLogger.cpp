5a0e61f623ddcedbd07185d1caae9352da007db7
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-13 02:47:34 +0500

    Update EventLogger.cpp

---
+++ EventLogger.cpp
@@ -0,0 +1,37 @@
+#include "EventLogger.h"
+
+EventLogger::EventLogger()
+{
+}
+
+void EventLogger::initialize(const MyStr& filename) {
+    string _filename = std::string(filename.c_str());
+    logfile.open(_filename);
+    if (!logfile)
+    {
+        cout << "This file can't be opened!";
+    }
+}
+void EventLogger:: cleanup() {
+
+    logfile.close();
+
+}
+void EventLogger::writeError(const MyStr& error_message) {
+    time_t timestamp;
+    time(&timestamp);
+
+    char timeStr[26];
+    ctime_s(timeStr, sizeof(timeStr), &timestamp);
+    logfile << "Error Time :: " << timeStr;
+    logfile << "Error :: " << error_message << endl;
+}
+void EventLogger::writeMessage(const MyStr& message) {
+    time_t timestamp;
+    time(&timestamp);
+
+    char timeStr[26];
+    ctime_s(timeStr, sizeof(timeStr), &timestamp);
+    logfile << "Event Time :: " << timeStr;
+    logfile << "Event :: " << message << endl;
+}
