4675bf7577d28ee5d8dffb5cba9a2c73649170ab
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-13 16:03:56 +0500

    Update Home.cpp

--- Home.cpp
+++ Home.cpp
@@ -1,15 +1,17 @@
 #include "Home.h"
 
-Home::Home(const MyStr& _name, const string& filename):name(_name)
+Home::Home(const MyStr& _name, const MyStr& filename, const MyStr& filename1):name(_name)
 {
 	this->eventlog.initialize(filename);
+	this->energylog.initialize(filename1);
+
 }
 
-Home* Home::getinstance(const MyStr& _name, const string& filename)
+Home* Home::getinstance(const MyStr& _name, const MyStr& filename, const MyStr& filename1)
 {
 	if (Main == nullptr)
 	{
-		Main = new Home(_name,filename);
+		Main = new Home(_name,filename,filename1);
 	}
 	return Main;
 }
@@ -17,11 +19,18 @@ Home* Home::getinstance(const MyStr& _name, const string& filename)
 void Home::AddUser(User* _user)
 {
 	this->users.push(_user);
+	MyStr name = _user->getname();
+	MyStr mess = name.append(" Added to Home!");
+	this->eventlog.writeMessage(mess);
 }
 
 void Home::AddRoom(Room* _room)
 {
 	this->rooms.push(_room);
+	MyStr name = _room->getname();
+	MyStr mess = name.append(" Added to Home!");
+	this->eventlog.writeMessage(mess);
+
 }
 
 void Home::AddDeviceToRoom(int _roomid, Device* _device)
@@ -30,29 +39,52 @@ void Home::AddDeviceToRoom(int _roomid, Device* _device)
 	{
 		int i = TempleFind(_roomid, this->rooms);
 		this->rooms[i]->AddDevice(_device);
+		MyStr devname = _device->getname();
+		
+		MyStr roomname = this->rooms[i]->getname();
+		MyStr mess = devname.append(" Added to ");
+		mess = mess.append(roomname);
+		this->eventlog.writeMessage(mess);
+
 	}
 	catch (const MyStr& error)
 	{
-		throw;
+		cout << error;
+		this->eventlog.writeError(error);
 	}
 }
 
 bool Home::ToggleDevicePower(int _roomid, int deviceid, MyStr username, bool toggle)
 {
-	int i = TempleFindbyName(username, this->users);
+	
 
 	try
 	{
+		int i = TempleFindbyName(username, this->users);
 		this->users[i]->CanToggleDevice();
-	
+		int j = TempleFind(_roomid, this->rooms);
+		if (!(this->rooms[j]->ToggleDevice(deviceid, toggle)))
+		{
+			try
+			{
+				this->rooms[j]->RoomEnergyUpdate(deviceid);
+			}
+			catch (const MyStr& update)
+			{
+				this->energylog.writeUpdate(update);
+			}
+		}
+		MyStr id = id.itos(deviceid);
+		MyStr mess = username.append(" toggled device id ").append(id).append(" of ").append(this->rooms[j]->getname());
+		this->eventlog.writeMessage(mess);
 
 	}
 	catch(const MyStr& error)
 	{
 		cout << error;
+		this->eventlog.writeError(error);
 	}
-	int j = TempleFind(_roomid, this->rooms);
-	this->rooms[j]->ToggleDevice(deviceid, toggle);
+	
 	return 1;
 }
 bool Home::ChangeSetting(int _roomid, int deviceid, MyStr username)
@@ -64,10 +96,52 @@ bool Home::ChangeSetting(int _roomid, int deviceid, MyStr username)
 		int j = TempleFind(_roomid, this->rooms);
 		this->rooms[j]->UpdateDeviceSetting(deviceid);
 
+		MyStr id = id.itos(deviceid);
+		MyStr mess = username.append(" changed some settings of device id ").append(id).append(" of ").append(this->rooms[j]->getname());
+		this->eventlog.writeMessage(mess);
 	}
 	catch (const MyStr error)
 	{
-		throw;
+		cout << error;
+		this->eventlog.writeError(error);
 	}
 	return 1;
+}
+
+Home::~Home()
+{
+	for (int i = 0; i < this->rooms.size(); i++)
+	{
+		delete this->rooms[i];
+	}
+}
+
+void Home::serialize(fstream& f)
+{
+	const char* newname = this->name.c_str();
+	int len = this->name.get_length(newname);
+	f.write((char*)&len, sizeof(len));
+	f.write(newname, len);
+	for (int i = 0; i < this->rooms.size(); i++)
+	{
+		this->rooms[i]->serialize(f);
+	}
+	for (int i = 0; i < this->users.size(); i++)
+	{
+		this->users[i]->serialize(f);
+	}
+}
+
+void Home::deserialize(fstream& f)
+{
+	int len;
+	f.read((char*)&len, sizeof(len));
+	char* newname = new char[len + 1];
+	f.read(newname, len);
+	newname[len] = '\0';
+	this->name.cpy(newname);
+	for (int i = 0; i < this->rooms.size(); i++)
+	{
+		this->rooms[i]->deserialize(f);
+	}
 }
