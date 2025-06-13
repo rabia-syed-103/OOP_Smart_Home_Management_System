8bc1cd953e821eccfd708c1165ea4fa6903f127e
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-13 16:04:34 +0500

    Update Home.h

--- Home.h
+++ Home.h
@@ -14,16 +14,21 @@ class Home
 	MyVector <Room*> rooms;
 	MyVector <User*> users;
 	EventLogger eventlog;
-	Home(const MyStr& _name, const string& filename);
+	EnergyMonitor energylog;
+
+	Home(const MyStr& _name, const MyStr& filename, const MyStr& filename1);
 	Home(const Home& other) = delete;
 
 public:
-	static Home* getinstance(const MyStr& _name, const string& filename);
+	static Home* getinstance(const MyStr& _name, const MyStr& filename, const MyStr& filename1);
 	void AddUser(User* _user);
 	void AddRoom(Room* _room);
 	void AddDeviceToRoom(int _roomid, Device* _device);
 	bool ToggleDevicePower(int _roomid, int deviceid, MyStr username,bool toggle);
 	bool ChangeSetting(int _roomid, int deviceid, MyStr username);
+	~Home();
+	void serialize(fstream& f);
+	void deserialize(fstream& f);
 
 
 };
