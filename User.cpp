977a08c7febead0d2c7c0cd3dd602aafe64f5035
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-13 16:11:04 +0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,5 +1,5 @@
 #include "User.h"
-
+#include <fstream>
 User::User()
 {
 }
@@ -7,7 +7,7 @@ User::User(MyStr _name) :name(_name)
 {
 
 }
-User::User(const User& other):name(other.name)
+User::User(const User& other) :name(other.name)
 {}
 User& User::operator=(const User& other)
 {
@@ -22,3 +22,23 @@ MyStr User::getname()
 {
 	return this->name;
 }
+
+void User::serialize(fstream& f)
+{
+	const char* newname = this->name.c_str();
+	int len = this->name.get_length(newname);
+	f.write((char*)&len, sizeof(len));
+	f.write(newname, len);
+}
+
+void User::deserialize(fstream& f)
+{
+	int len;
+	f.read((char*)&len, sizeof(len));
+	char* newname = new char[len + 1];
+	f.read(newname, len);
+	newname[len] = '\0';
+	this->name.cpy(newname);
+	
+}
+
