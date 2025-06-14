3671259b63392104c8b9f6ed8d56e376ab389637
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-14 15:20:23 +0500

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -5,11 +5,18 @@ class Sensor :public Device
 {
 public:
 	Sensor();
-	Sensor(int _id,MyStr _name, bool _isON);
+	Sensor(int _id,MyStr _name);
 	Sensor(const Sensor& other);
 	Sensor& operator=(const Sensor& other);
-	void turnOn() override;
-	void turnOff() override;
+	void turnOn();
+	void turnOff();
 	void SetSetting() override;
+	void EnergyUsed()override;
+	void serialize(fstream& f)override;
+	void deserialize(fstream& f)override;
+	void detectEvent();
+	~Sensor();
+	void Display()override;
+	void RemoveDevice()override;
 };
 #endif // !Sensor_H
