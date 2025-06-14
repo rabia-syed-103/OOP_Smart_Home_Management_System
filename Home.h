f57f7a06bddb8e858610ec3faf67089db1a50a24
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-14 15:08:51 +0500

    Update Home.h

--- Home.h
+++ Home.h
@@ -7,6 +7,7 @@
 #include "MyStr.h"
 #include "MyVector.h"
 #include "Room.h"
+#include "Sensor.h"
 class Home
 {
 	static Home* Main;
@@ -23,13 +24,20 @@ public:
 	static Home* getinstance(const MyStr& _name, const MyStr& filename, const MyStr& filename1);
 	void AddUser(User* _user);
 	void AddRoom(Room* _room);
-	void AddDeviceToRoom(int _roomid, Device* _device);
+	void roomidprints();
+	void useridprints();
+	void AddDeviceToRoom(int _roomid, Device* _device,MyStr _username);
+	void RemoveDeviceFromRoom(int _roomid, int _device, MyStr _username);
 	bool ToggleDevicePower(int _roomid, int deviceid, MyStr username,bool toggle);
 	bool ChangeSetting(int _roomid, int deviceid, MyStr username);
 	~Home();
 	void serialize(fstream& f);
 	void deserialize(fstream& f);
-
+	void Display();
+	void printEventlog();
+	void printEnergyLog();
+	void triggerSensor(int roomName, Sensor* sensor);
+	void Clear();
 
 };
 #endif // !HOME_H
