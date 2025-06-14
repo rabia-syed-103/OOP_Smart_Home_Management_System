07d671f34f5e37db25953c831dede33fe84590f4
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-14 15:19:49 +0500

    Update Sensor.cpp

--- Sensor.cpp
+++ Sensor.cpp
@@ -2,14 +2,18 @@
 
 Sensor::Sensor()
 {
+
 }
-Sensor::Sensor(int _id,MyStr _name, bool _isON) :Device(_id,_name, _isON)
+
+Sensor::Sensor(int _id,MyStr _name) :Device(_id,_name, true,20)
 {
 
 }
+
 Sensor::Sensor(const Sensor& other) :Device(other)
 {
 }
+
 Sensor& Sensor::operator=(const Sensor& other)
 {
 	if (this != &other)
@@ -30,8 +34,48 @@ void Sensor::turnOff()
 	throw MyStr("Sensor cannot be turned off manually.");
 
 }
+
 void Sensor::SetSetting()
 {
 	throw MyStr("Settings of Sensor cannot be changed.");
 
 }
+
+void Sensor::EnergyUsed()
+{
+	throw("Energy of sensor cannot be monitored!");
+}
+
+void Sensor::serialize(fstream& f)
+{
+	this->_serialize(f);
+
+}
+
+void Sensor::deserialize(fstream& f)
+{
+	this->_deserialize(f);
+
+}
+
+Sensor::~Sensor()
+{
+}
+
+void Sensor::Display()
+{
+	this->print();
+	cout << endl << endl;
+
+}
+void Sensor::RemoveDevice()
+{
+	throw MyStr("\nSensor Can't be Removed!\n");
+}
+void Sensor:: detectEvent() {
+	MyStr message = "Sensor ";
+	message = message.append(this->getname()).append(" triggered in ");
+
+	throw MyStr (message);	
+}
+
