42e3524bf7e813a46696505de50b651ffd1356c7
Rabia-Syed-103 <bscs24103@itu.edu.pk>
2025-06-14 15:11:30 +0500

    Update Lock.h

--- Lock.h
+++ Lock.h
@@ -6,12 +6,16 @@ class Lock :public Device
 	MyStr password;
 public:
 	Lock();
-	Lock(int _id,MyStr _name, bool _isON);
+	Lock(int _id,MyStr _name, bool _isON,MyStr _pass);
 	Lock(const Lock& other);
 	Lock& operator=(const Lock& other);
-	void turnOn() override;
-	void turnOff() override;
+	void turnOn();
+	void turnOff();
 	void SetSetting() override;
+	void EnergyUsed()override;
+	void serialize(fstream& f)override;
+	void deserialize(fstream& f)override;
+	~Lock();
+	void Display()override;
 };
 #endif // !LOCK_H
-
